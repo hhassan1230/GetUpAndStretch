8d0f7d1d6042691a7840185e61c4bb65dfbfc31d
Carlos Ravelo <carlos.ravelo@apps.ask.com>
2016-11-03 12:39:44 -0400

    Added a utility function to reply with the instance name

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const secrets = require('./secrets.local');
 const moment = require('moment');
 const botkit = require('botkit');
+const os = require('os');
 
 const onehour = 1000 * 60 * 60;
 
@@ -13,28 +14,46 @@ if (!secrets.API_KEY) {
 // local state
 const state = {
     userNickName: {},
-    timeCandyLastFilled: null
+    timeCandyLastFilled: null,
+    upTimestamp: new Date(),
 };
 
+// setup
 const controller = botkit.slackbot({
     debug: false
 });
 
 // connect the bot to a stream of messages
-controller.spawn({
+var botInstance = controller.spawn({
     token: secrets.API_KEY
-}).startRTM();
+}).startRTM(function () {
+    console.log('Done initializing.');
+});
+
+// utilities
+function sendMessage(message, reply) {
+    botInstance.reply(message, secrets.botInstanceName + reply);
+}
 
 // give the bot something to listen for.
-controller.hears('hello', ['direct_message', 'direct_mention'], function (bot, message) {
-    bot.reply(message, 'Hello yourself, I am alive!');
-});
+controller.hears(
+    ['uptime', 'identify yourself', 'who are you', 'what is your name'],
+    'direct_message,direct_mention,mention', function(bot, message) {
+        var hostname = os.hostname();
+        var uptime = moment(state.upTimestamp).fromNow();
+
+        sendMessage(message,
+            ':robot_face: I am a bot named <@' + bot.identity.name +
+            '>. I have been running for ' + uptime + ' on ' + hostname + '.'
+        );
+    }
+);
 
 controller.hears('what up mofo', ['direct_message', 'direct_mention'], function (bot, message) {
     setTimeout(punkReply, 2000);
 
     function punkReply() {
-        bot.reply(message, 'Look here motherfuka! I ain\'t got time for your punk ass');
+        sendMessage(message, 'Look here motherfuka! I ain\'t got time for your punk ass');
     }
 });
 
@@ -43,14 +62,10 @@ controller.hears('restocked', ['direct_message','direct_mention'], function(bot,
 
     state.timeCandyLastFilled = +(new Date());
 
-    bot.reply(message, secrets.botInstanceName + 'Thanks for notifying me that there is delicious candy in the kitchen.  I will inform everyone.');
+    sendMessage(message, secrets.botInstanceName + 'Thanks for notifying me that there is delicious candy in the kitchen.  I will inform everyone.');
 });
 
 controller.hears('candy', ['direct_message','direct_mention'], function(bot, message) {
-
-    function sendMessage(msg) {
-        bot.reply(message, secrets.botInstanceName + msg);
-    }
 
     if (!state.timeCandyLastFilled) {
         sendMessage('I have no idea when the candy was last filled.  Why don\'t you just get up and see for yourself!');
@@ -72,27 +87,40 @@ controller.hears('candy', ['direct_message','direct_mention'], function(bot, mes
 });
 
 // Name calling
-controller.hears('call me', ['direct_message', 'direct_mention'], function (bot, message) {
-    state.userNickName[message.user] = message.text.replace('call me ', '');
+controller.hears([/^call me ?(.*)?$/, /^my name is ?(.*)?$/], ['direct_message', 'direct_mention'], function (bot, message) {
+    const nickName = message.match[1];
+    console.log(message.match);
 
-    console.log(message);
+    if (!nickName) {
+        sendMessage(message, 'Call you what?? What did you want me to fucking call you?');
 
-    bot.reply(message, 'You don\'t tell me, what to do motherfucker!...');
-
-    setTimeout(function () {
-        bot.reply(message, 'ok fine, I\'ll call you ' + state.userNickName[message.user]);
-    }, 2000);
-});
-
-controller.hears(/who am i\??/, ['direct_message', 'direct_mention'], function (bot, message) {
-    console.log(state.userNickName);
-
-    if (state.userNickName[message.user]) {
-        bot.reply(message, 'You fucking told me before your name was ' + state.userNickName[message.user]);
+        if (state.userNickName[message.user]) {
+            sendMessage(message, 'I\'ll keep calling you ' + state.userNickName[message.user]);
+        }
     }
     else {
-        bot.reply(message, 'You haven\'t fucking told me your name yet. You can tell me by saying: `call me <name>`');
+        state.userNickName[message.user] = nickName;
+        sendMessage(message, 'You don\'t tell me, what to do motherfucker!...');
+
+        setTimeout(function () {
+            sendMessage(message, 'ok fine, I\'ll call you ' + state.userNickName[message.user]);
+        }, 2000);
     }
+});
+
+function whoami(message) {
+    if (state.userNickName[message.user]) {
+        return 'You fucking told me before your name was ' + state.userNickName[message.user];
+    }
+    else {
+        return 'You haven\'t fucking told me your name yet. You can tell me by saying: `call me <name>`';
+    }
+}
+
+controller.hears(/who am i\??/, ['direct_message', 'direct_mention'], function (bot, message) {
+    const reply = whoami(message);
+
+    sendMessage(message, reply);
 });
 
 controller.hears(['check', 'see', 'how', 'funny'], ['direct_message', 'direct_mention'], function (bot, message) {
