c6337f0926903eb4f25a8a705391d25edd500ad8
Carlos Ravelo <carlos.ravelo@apps.ask.com>
2016-11-03 10:41:15 -0400

    Restoring secrets implementation

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
+const secrets = require('./secrets.local');
+const moment = require('momentjs');
 const botkit = require('botkit');
 
 let timeCandyLastFilled = null;
 
+// local state
+const state = {
+    userNickName: null
+};
+
 const controller = botkit.slackbot({
-  debug: true,
+    debug: false
 });
 
 // connect the bot to a stream of messages
 controller.spawn({
-  token: ''
+    token: secrets.API_KEY
 }).startRTM();
 
 // give the bot something to listen for.
-controller.hears('hello', ['direct_message','direct_mention'], function(bot,message) {
-  bot.reply(message, 'Hello yourself, I am alive!');
+controller.hears('hello', ['direct_message', 'direct_mention'], function (bot, message) {
+    bot.reply(message, 'Hello yourself, I am alive!');
 });
-controller.hears('what up mofo', ['direct_message','direct_mention'], function(bot,message) {
-	setTimeout(punkReply, 2000);
 
-	function punkReply(){
-		bot.reply(message, 'Look here motherfuka! I ain\'t got time for your punk ass');
-	}
+controller.hears('what up mofo', ['direct_message', 'direct_mention'], function (bot, message) {
+    setTimeout(punkReply, 2000);
+
+    function punkReply() {
+        bot.reply(message, 'Look here motherfuka! I ain\'t got time for your punk ass');
+    }
 });
 
 controller.hears('restocked', ['direct_message','direct_mention'], function(bot,message) {
@@ -32,4 +40,14 @@ controller.hears('restocked', ['direct_message','direct_mention'], function(bot,
 controller.hears('candy', ['direct_message','direct_mention'], function(bot,message) {
 	console.log('Candy request received');
 	bot.reply(message, 'The time the candy jars were filled last in the kitchen was' + timeCandyLastFilled);
+});
+
+controller.hears('call me', ['direct_message', 'direct_mention'], function (bot, message) {
+    let out = '';
+    const myConsole = new console.Console(out, err);
+
+    myConsole.log('bot:', bot);
+    myConsole.log('message:', message);
+
+    bot.reply(out);
 });
