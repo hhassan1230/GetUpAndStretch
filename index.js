f00b07bca2b4b687907902e16b7a84efe33e8d29
Carlos Ravelo <carlos.ravelo@apps.ask.com>
2016-11-03 10:28:41 -0400

    Added a secrets file and moment

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-//
-// TODO: Replace <my_slack_bot_token> with the API token from the Slack Slackbot Integration configuration
-//
+const secrets = require('./secrets.local');
+const moment = require('momentjs');
 
 const botkit = require('botkit');
 
 const controller = botkit.slackbot({
-  debug: false,
+  debug: false
 });
 
 // connect the bot to a stream of messages
 controller.spawn({
-  token: 'TOKEN_GOES_HERE'
+  token: secrets.API_KEY
 }).startRTM();
 
 // give the bot something to listen for.
 controller.hears('hello', ['direct_message','direct_mention'], function(bot,message) {
-
   bot.reply(message, 'Hello yourself, I am alive!');
 });
+
 controller.hears('what up mofo', ['direct_message','direct_mention'], function(bot,message) {
 	setTimeout(punkReply, 2000);
 
